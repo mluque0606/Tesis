c17eff26fd5f93a41a959e45ceb101e098169185
mluque0606 <115661586+mluque0606@users.noreply.github.com>
2024-03-26 09:00:33 -0300

    Complete página de referencia

--- src/ReferencePage.js
+++ src/ReferencePage.js
@@ -49,13 +49,19 @@ function ReferencePage({ onClose }) {
                   <h2> Características del problema: </h2>
                   <div className='reference-characteristics'>
                     <p>
-                      <strong>Expresión en Forma de Tupla:</strong> La solución se representa como una tupla (x1, x2, ..., xn), lo que permite explorar diferentes combinaciones de valores.
+                      <strong>Espacio de Soluciones:</strong> El espacio de soluciones puede modelarse mediante un árbol binario o n-ario que contiene todas las posibles secuencias de decisiones. Donde cada nodo representa un estado del problema y los arcos las decisiones tomadas para cambiar de estado.
+                    </p>
+                    <p>
+                      <strong>Expresión en Forma de Tupla:</strong> La solución puede expresarse como una n-tupla (x1, x2, ..., xn), donde los Xi pertenencen a un cierto dominio.
                     </p>
                     <p>
                       <strong>Función Factible:</strong> Evalúa si una tupla cumple con las restricciones del problema, ayudando a descartar soluciones inviables.
                     </p>
                     <p>
-                      <strong>Función Solución:</strong> Determina si una tupla factible es una solución válida para el problema, identificando resultados útiles en la búsqueda.
+                      <strong>Función Solución:</strong> Determina si una tupla factible es una solución válida para el problema.
+                    </p>
+                    <p>
+                      <strong>Función Objetivo:</strong> Es la función a optimizar o satisfacer.
                     </p>
                     <p>
                       <strong>Optimización:</strong> La técnica puede utilizarse para optimizar o satisfacer objetivos específicos, como minimizar costos o cumplir con restricciones.
@@ -63,8 +69,8 @@ function ReferencePage({ onClose }) {
                     <p>
                       <strong>Restricciones del problema:</strong> 
                     </p>
-                    <p>• Explícitas: describen el dominio de los xi.</p>
-                    <p>• Implícitas: describen las relaciones que deben cumplirse entre los xi. Determinan qué tuplas dentro del espacio de soluciones satisfacen la función objetivo.</p>
+                    <p>   • Explícitas: describen el dominio de los Xi.</p>
+                    <p>   • Implícitas: describen las relaciones que deben cumplirse entre los xi. Determinan qué tuplas dentro del espacio de soluciones satisfacen la función objetivo.</p>
                   </div>
               </section>
 
@@ -75,17 +81,17 @@ function ReferencePage({ onClose }) {
                       <strong>1)</strong> Forma cada tupla de manera progresiva.
                     </p>
                     <p>
-                      <strong>2)</strong> Verifica si cada xi añadido a la tupla (x1,x2,..,xi) conduce a una solución factible.
+                      <strong>2)</strong> Verifica si cada Xi añadido a la tupla (x1,x2,..,xi) conduce a una solución factible.
                     </p>
                     <p>
                       <strong>3)</strong> Si FACTIBLE (x1,x2,..,xi) = FALSO
-                      <p>• Corta la búsqueda</p>
-                      <p>• Prueba con otro valor válido de xi</p>
-                      <p>• Si no existen valores válidos retrocede</p>
+                      <p>   • Corta la búsqueda</p>
+                      <p>   • Prueba con otro valor válido de Xi</p>
+                      <p>   • Si no existen valores válidos retrocede</p>
                     </p>
                     <p>
                       <strong>4)</strong> Si FACTIBLE (x1,x2,..,xi) = VERDADERO
-                      <p>• Repite el procedimiento para incorporar x i+1 a la tupla</p>
+                      <p>   • Repite el procedimiento para incorporar Xi+1 a la tupla</p>
                     </p>
                   </div>
               </section>
@@ -125,6 +131,14 @@ function ReferencePage({ onClose }) {
                     <p> La complejidad temporal representa la cantidad de tiempo que un algoritmo tarda en ejecutarse en función del tamaño de la entrada, 
                       este se puede ver afectado principalmente por la cantidad de operaciones básicas ejecutadas por el algoritmo. Ademas, permite comparar 
                       la eficiencia relativa de diferentes algoritmos para resolver un mismo problema</p>
+                  </div>
+                  <h2>• Empírica </h2>
+                  <div className='reference-characteristics'>
+                    <p> El análisis empírico implica medir el tiempo de ejecución real del algoritmo en un entorno específico, lo que incluye el hardware y el contexto de ejecución; este enfoque proporciona una evaluación práctica del rendimiento del algoritmo pero puede verse influenciado por factores externos. Su resultado se mide en unidades de tiempo</p>
+                  </div>
+                  <h2>• Teórica </h2>
+                  <div className='reference-characteristics'>
+                    <p> El análisis teórico se basa en la cantidad de operaciones básicas que realiza el algoritmo en relación con el tamaño de la entrada; este análisis es independiente del contexto de ejecución y proporciona una comprensión abstracta del rendimiento del algoritmo en diferentes tamaños de entrada. Su resultado se mide en cantidad de operaciones elementales.</p>
                   </div>
               </section>
 
